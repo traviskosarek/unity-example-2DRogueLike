cc5c0c7c2ebaea9b533c302a88d1023d085003a7
Travis Kosarek <travis.kosarek@gmail.com>
2018-01-27 10:02:34 -0600

    initial commit of completed example

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    public AudioClip chopSound1;
+    public AudioClip chopSound2;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+	public void DamageWall(int loss){
+        spriteRenderer.sprite = dmgSprite;
+        SoundManager.instance.RandomizeSfx(chopSound1, chopSound2);
+        hp -= loss;
+		if(hp <= 0){
+            gameObject.SetActive(false);
+        }
+    }
+}
